930126cbb4f96b85d587f6c80699dec9178fee0a
HAFSAFa <h.nayah@edu.umi.ac.ma>
2024-11-19 11:27:19 +0100

    Initial React App

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState('');
+
+  const addTask = () => {
+    if (task.trim()) {
+      setTasks([...tasks, task]);
+      setTask('');
+    }
+  };
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        placeholder="Enter a task"
+      />
+      <button onClick={addTask}>Add Task</button>
+      <ul>
+        {tasks.map((t, index) => (
+          <li key={index}>{t}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
